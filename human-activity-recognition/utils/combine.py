f23884043b9979001d27e06148e693d6b8c5cf7c
Mika Hoppe <mika@awork.com>
2025-05-11 10:10:35 +0200

    feat(human activity recognition): normalize data to zero mean and unit variance

--- human-activity-recognition/utils/combine.py
+++ human-activity-recognition/utils/combine.py
@@ -4,6 +4,15 @@ from .sensors import sensors
 from .upsample import upsample
 
 
+def normalize(data):
+    """
+    Normalize the data
+    """
+    data = data - data.mean(dim=2, keepdim=True)
+    data = data / data.std(dim=2, keepdim=True)
+    return data
+
+
 def combine(is_test=False):
     """
     Combine sensor data into a tensor
@@ -12,6 +21,7 @@ def combine(is_test=False):
 
     for sensor in sensors:
         data = sensor.get_data_as_tensor(test=is_test)
+        data = normalize(data)
 
         if out is None:
             out = data
